8eec5312a37c66845cf27063b7ce9ae079f94c82
MohammadHakim <mohrashak@gmail.com>
2016-06-25 04:49:41 -0400

    remove commented out code

--- notification.py
+++ notification.py
@@ -1,50 +1,5 @@
 #this file will host my try to notify me of updates Using A web Parser 
 #parts of this code were contributed by programmers on http://stackoverflow.com/
-"""
-def getTitlesFrom(String):
-    #for that I will need the number of lines
-    myfile = open(String,'r+')
-    lines = sum(1 for line in myfile)
-    myfile.close()
-    #to get the list of titles
-    myfile = open('MangA.txt','r+')
-    titles = []
-    for title in range(lines):
-        titles.append(myfile.readline().split('\t')[0])
-    myfile.close()
-    return titles
-def getOldCh(String):
-    import re
-    myfile = open(String,'r+')
-    lines = sum(1 for line in myfile)
-    myfile.close()
-    
-    myfile = open(String,'r+')
-    old = []
-    for title in range(lines):
-        old.append(re.search('\d+\.\d+|\d+',myfile.readline().split('\t')[1]).group(0)) 
-    myfile.close()
-    for title in range(lines):
-        old[title] = float(old[title])
-    return old
-def getHtmlOf(title):
-    #hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-    #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    #   'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    #   'Accept-Encoding': 'none',
-    #   'Accept-Language': 'en-US,en;q=0.8',
-    #   'Connection': 'keep-alive'}
-    #import urllib2
-    import cfscrape
-    scraper = cfscrape.create_scraper()
-    website = "http://kissmanga.com/Manga/"
-    website = website+title
-    #request = urllib2.Request(website,headers=hdr)
-    #response = urllib2.urlopen(request)
-    #page_source = response.read()
-    page_source = scraper.get(website).content
-    return page_source
-"""
 def getOldTitleNChap(String):
     myfile = open(String,'r+')
     titles = []
@@ -69,16 +24,6 @@ def extractChapterNum(choice, title):
     """This function looks for chapter, ch or numbers in a given choice string
     and utilizes the page title to choose candidate numbers that are assumed to
     be the chapter number"""
-    ###I need to create a way to find the chapter numbers
-    ##chapter1 ='Yamada-kun to 7-nin no Majo Yamada-kun to 007-nin no Majo 090'
-    ##title1 = 'Yamada-kun to 7-nin no Majo manga | Read Yamada-kun to 7-nin no Majo manga online in high quality'
-    ##chapter2 = 'Trump (LEE Chae-Eun) Ch.029: The 073rd Cabinet (019)'
-    ##title2 = 'Trump (LEE Chae-Eun) manga | Read Trump (LEE Chae-Eun) manga online in high quality'
-    ##chapter3 = 'Magician Vol.002.029 Ch.127: Long Night (006)'
-    ##title3 = 'Magician manga | Read Magician manga online in high quality'
-    ##chapter4 ='Otoyomegatari Vol.006 Ch.034: Shield at Your Back'
-    ##title4 ='Otoyomegatari manga | Read Otoyomegatari manga online in high quality'
-
     #the easiest way to find the chapter number is to look for the two variations ch and chapter
     import re
     #first convert the string to all lower case to avoid the need to account to variations
@@ -97,11 +42,9 @@ def extractChapterNum(choice, title):
     else:
         #first try to find the word chapter in the title
         chapter = choice.split('ch')
-        #print chapter[0]
         if len(chapter)>1:
             #If you fall here then the word chapter was found, search in the 1st term for the chapter number
             for n in range(1,len(chapter)):
-                #print chapter[n]
                 try:
                     chapter = re.search("\d+|\d+\.\d+",chapter[n]).group(0)
                     break
@@ -109,7 +52,7 @@ def extractChapterNum(choice, title):
                     chapter = 1000#meaning there is a problem
         else:
             #If you fall here then chapter was not found try ch instead
-            chapter = choice.split('chapter');#print chapter;#CH IN TITLE PROBLEM
+            chapter = choice.split('chapter');#CH IN TITLE PROBLEM
             if len(chapter)>1:
                 #if you fall here then ch was found, search for the chapter number
                 for n in range(1,len(chapter)):
@@ -128,13 +71,11 @@ def extractChapterNum(choice, title):
                 newchapter = []
                 for number in chapter:
                     for occurance in title:
-                        #print number,occurance,float(number)!= float(occurance)
                         if(float(number)== float(occurance)):
                             break
                         else:
                             if occurance == (title[len(title)-1]):
                                 newchapter.append(number)
-                #print newchapter
                 if(len(newchapter)==1):
                     chapter = newchapter[0]
                 elif(len(newchapter)==0):
@@ -143,11 +84,6 @@ def extractChapterNum(choice, title):
                     chapter = 1000#meaning there is a problem
     chapter = float(chapter)
     return chapter
-"""
-def listNewChapters(titles):
-	new = []
-	return new
- """
 def CheckNprint(titles,old,textFile):
 	try:
          	import datetime
@@ -194,13 +130,10 @@ def CheckNprint(titles,old,textFile):
 
 
 ####################################################################################
-#first step is to read in the titles
 from time import sleep
 print "Program Start"
-"""
-titles = getTitlesFrom('MangA.txt')
-old = getOldCh('MangA.txt')
-"""
+
+#first step is to read in the titles
 titles, old = getOldTitleNChap('MangA.txt')
 #now let me try to get a list of the new titles from kissmanga.com
 CheckNprint(titles,old,'UpdatedManga.txt')
