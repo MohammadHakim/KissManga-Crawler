59b45b895eeb25d9df0939a96c6af0a5cf122073
MohammadHakim <mohrashak@gmail.com>
2016-12-21 17:47:08 -0500

    unify indentations

--- notification.py
+++ notification.py
@@ -15,11 +15,11 @@ def getHtmlOf(title,scraper):
     page_source = scraper.get(website).content
     return page_source
 def getString(page_source):
-	from bs4 import BeautifulSoup
-	soup = BeautifulSoup(page_source)
-	Chapter = soup.td.a.get_text()
-	Title = soup.title.get_text()
-	return Chapter,Title
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(page_source)
+    Chapter = soup.td.a.get_text()
+    Title = soup.title.get_text()
+    return Chapter,Title
 def extractChapterNum(choice, title):
     """This function looks for chapter, ch or numbers in a given choice string
     and utilizes the page title to choose candidate numbers that are assumed to
@@ -43,52 +43,52 @@ def extractChapterNum(choice, title):
             chapter = 1000
     return float(chapter)
 def CheckNprint(titles,old,textFile):
-	try:
-         	import datetime
-         	import cfscrape
-		from bs4 import BeautifulSoup
-		scraper = cfscrape.create_scraper()
-         	f = open(textFile,'a')
-         	f.truncate()
-        	f.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n')
-        	new = []
-        	n = 0
-     		total = 0
-         	while n < len(titles):
-        		print "Checking "+titles[n]
-			page_source = getHtmlOf(titles[n],scraper)
-        		Chapter,Title = getString(page_source)
-        		Number = extractChapterNum(Chapter,Title)
-			#handle chapter titles with no numbers (1000 indicates an error)
-			if Number == 1000:
-			    soup = BeautifulSoup(page_source)
-			    listOfChapters = [x for x in soup.find_all('td') if x.a !=None]
-			    Number = next((i+old[n] for i,x in enumerate(listOfChapters) if extractChapterNum(x.a.get_text(),Title) == old[n]),1000)
-        		new.append(Number)
-        		if(Number>old[n]):
-        			maybe = "New Chapter"+"("+'%5s'+") "+'%5s'+" for"+'%30s'+". You are now on "+ '%5s'     
-        			f.write(maybe % (str(Number-old[n]),str(Number),str(titles[n]),str(old[n])) + '\n')
-        			print maybe % (str(Number-old[n]),str(Number),str(titles[n]),str(old[n]))
-        			total = total + Number-old[n]
-        			if eval(str(total).split(".")[1]) > 0:
-        				total = eval(str(total).split(".")[0]) + 1
-        		n+=1
-        	print 4*'\n'
-        	print "Total: ",total
-        	print "See ", textFile," for report"
-        	f.write('\n'+'Total: '+str(total))		
-        	f.write((4*'\n'))
-        	f.write("List of titles with updated chapter numbers:\n")
-        	for n in range(len(titles)):
-        		f.write(str(titles[n]))
-        		f.write(str('\t'))
-        		f.write(str(new[n]))
-        		f.write(str('\n'))
-        	f.close()
-	except:
-        	f.write("error")
-        	f.close()
-        	raise	
+    try:
+        import datetime
+        import cfscrape
+        from bs4 import BeautifulSoup
+        scraper = cfscrape.create_scraper()
+        f = open(textFile,'a')
+        f.truncate()
+        f.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n')
+        new = []
+        n = 0
+        total = 0
+        while n < len(titles):
+            print "Checking "+titles[n]
+            page_source = getHtmlOf(titles[n],scraper)
+            Chapter,Title = getString(page_source)
+            Number = extractChapterNum(Chapter,Title)
+            #handle chapter titles with no numbers (1000 indicates an error)
+            if Number == 1000:
+                soup = BeautifulSoup(page_source)
+                listOfChapters = [x for x in soup.find_all('td') if x.a !=None]
+                Number = next((i+old[n] for i,x in enumerate(listOfChapters) if extractChapterNum(x.a.get_text(),Title) == old[n]),1000)
+            new.append(Number)
+            if(Number>old[n]):
+                maybe = "New Chapter"+"("+'%5s'+") "+'%5s'+" for"+'%30s'+". You are now on "+ '%5s'     
+                f.write(maybe % (str(Number-old[n]),str(Number),str(titles[n]),str(old[n])) + '\n')
+                print maybe % (str(Number-old[n]),str(Number),str(titles[n]),str(old[n]))
+                total = total + Number-old[n]
+                if eval(str(total).split(".")[1]) > 0:
+                    total = eval(str(total).split(".")[0]) + 1
+            n+=1
+        print 4*'\n'
+        print "Total: ",total
+        print "See ", textFile," for report"
+        f.write('\n'+'Total: '+str(total))		
+        f.write((4*'\n'))
+        f.write("List of titles with updated chapter numbers:\n")
+        for n in range(len(titles)):
+            f.write(str(titles[n]))
+            f.write(str('\t'))
+            f.write(str(new[n]))
+            f.write(str('\n'))
+        f.close()
+    except:
+        f.write("error")
+        f.close()
+        raise	
 	
 
 
